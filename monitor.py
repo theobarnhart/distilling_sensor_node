4948d76541f5bbe5097e97f0471a5ada5950aefe
theobarnhart <theodore.barnhart@colorado.edu>
2018-10-15 21:10:01 -0600

    monitor and print functions added.

--- monitor.py
+++ monitor.py
@@ -1,12 +1,26 @@
 from common import *
 import board
+import time
 
 # parameters
 upperPin = board.D5
+lowerPin = board.D6
 #lowerPin = board.D6?
 db = '/home/pi/distDat.db'
 upperTemp = 'upperTemp'
 lowerTemp = 'lowerTemp'
+fontsize=22
 
-insert_db(db, upperTemp, read_temperature(upperPin)) # read upper temperature
-#insert_db(db, lowerTemp, read_temperature(lowerPin)) # read the lower temperature
+disp,draw,font,width,height,image = initialize_display(fontsize)
+
+while True:
+    lowerReading = read_temperature(lowerPin)
+    upperReading = read_temperature(upperPin)
+    
+    insert_db(db, upperTemp, upperReading) # read upper temperature
+    insert_db(db, lowerTemp, lowerReading) # read the lower temperature
+    
+    print_message(["U: %s"%(upperReading),"L: %s"%(lowerReading)],disp,draw,font,width,height,image)
+    
+    time.sleep(10)
+    
