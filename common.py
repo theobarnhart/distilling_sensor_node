4b02b7f7edf2cb81031a4506c5c7b0ac665fbcda
theobarnhart <theodore.barnhart@colorado.edu>
2018-10-15 20:07:32 -0600

    bug fix

--- common.py
+++ common.py
@@ -90,7 +90,7 @@ def print_message(message,disp,draw,font,width,height):
 
     # Write two lines of text.
     for l in message:
-        draw.text((x, top),l,  font=font, fill=255)
+        draw.text((0, top),l,  font=font, fill=255)
         top += 8
 
     # Display image.
