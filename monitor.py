ef289fdab7cf107d177615f32bf6f1b603e90f5b
Theodore Barnhart <theo.barnhart@gmail.com>
2020-01-02 19:31:46 -0700

    implemented logging and printing to consol, but only in debug mode.

--- monitor.py
+++ monitor.py
@@ -35,7 +35,7 @@ except Exception as e:
 time.sleep(config.delay) # delay to let the internet connect
 
 if not config.runProgram:
-	print('Exiting Program.')
+	if debug: print('Exiting Program.')
 	sys.exit()
 
 try: # setup logging:
@@ -51,6 +51,9 @@ try: # setup logging:
 	logging.info('Logging initiated.')
 except Exception as e:
 	logging.info('Logging failed.')
+	logging.info(e)
+	logging.info(traceback.format_exc())
+
 	print('Logging failed.')
 	print(e)
 	print(traceback.format_exc())
@@ -79,9 +82,14 @@ try:
 
 	if debug: print('Initialization Complete')
 except Exception as e:
-	print('Datalogging initialization failed.')
-	print(e)
-	print(traceback.format_exc())
+	if debug:
+		logging.info('Datalogging initialization failed.') 
+		logging.info(e)
+		logging.info(traceback.format_exc())
+
+		print('Datalogging initialization failed.')
+		print(e)
+		print(traceback.format_exc())
 
 lastReading = 70
 
@@ -93,14 +101,20 @@ while True: # run the monitoring function
 		now = datetime.datetime.fromtimestamp(time.mktime(localtime)) # convert to datetime
 		#if debug: print('%s:%s'%(minutes,seconds))
 	except Exception as e:
-		print('Time acquisition failed.')
-		print(e)
-		print(traceback.format_exc())
+		if debug:
+			logging.info('Time acquisition failed.')
+			logging.info(e)
+			logging.info(traceback.format_exc())
+			
+			print('Time acquisition failed.')
+			print(e)
+			print(traceback.format_exc())
 
 	try:
 		if any(int(seconds) == x for x in config.display_update): # Display update loop
 			try:
-				if debug: logging.info('Updating display.')
+				if debug:
+					logging.info('Updating display.')
 				# read the upper and lower temperatures and display
 				upperT = read_temperature(upperPin)
 				lowerT = read_temperature(lowerPin)
@@ -113,32 +127,57 @@ while True: # run the monitoring function
 
 					if lastReading != minutes:
 						try:
-							if debug: logging.info('Logging Data.')
+							if debug:
+								logging.info('Logging Data.')
 
 							fmtTime = time.strftime(config.time_fmt,localtime)
 							temp,voc,eco2 = read_atm()
 							flow = read_flow()
 
-							print('**********')
-							print(fmtTime)
-							print("Upper Temperature: %.2f"%upperT)
-							print("Lower Temperature: %.2f"%lowerT)
-							print('ATM temp.: %.2f'%(temp))
-							print('ATM voc: %.2f'%(voc))
-							print('ATM eco2: %.2f'%(eco2))
-							print('Flow: %.2f'%(flow))
+							if debug:
+								print('**********')
+								print('Sensor Data: %s'%fmtTime)
+								print("Upper Temperature: %.2f"%upperT)
+								print("Lower Temperature: %.2f"%lowerT)
+								print('ATM temp.: %.2f'%(temp))
+								print('ATM voc: %.2f'%(voc))
+								print('ATM eco2: %.2f'%(eco2))
+								print('Flow: %.2f'%(flow))
+
+								logging.info('**********')
+								logging.info('Sensor Data: %s'%fmtTime)
+								logging.info("Upper Temperature: %.2f"%upperT)
+								logging.info("Lower Temperature: %.2f"%lowerT)
+								logging.info('ATM temp.: %.2f'%(temp))
+								logging.info('ATM voc: %.2f'%(voc))
+								logging.info('ATM eco2: %.2f'%(eco2))
+								logging.info('Flow: %.2f'%(flow))
+
 						except Exception as e:
-							print('atm data failed.')
-							print(e)
+							if debug: 
+								logging.info('atm data failed.')
+								logging.info(e)
+								logging.info(traceback.format_exc())
+
+								print('atm data failed.')
+								print(e)
+								print(traceback.format_exc())
 
 						try:
 							data = [fmtTime, round(upperT,2), round(lowerT,2), round(flow,2), round(voc,2), round(eco2,2)] # data for google sheets
 						except Exception as e:
-							print('Data Construct Failed')
-							print(e)
-							print(traceback.format_exc())
+							if debug:
+								logging.info('Data Construct Failed.')
+								logging.info(e)
+								logging.info(traceback.format_exc())
+
+								print('Data Construct Failed.')
+								print(e)
+								print(traceback.format_exc())
 
 						try:
+							if debug:
+								logging.info('Checking gc credential time.')
 							if (time.localtime() - gcTime)/60. > 55: # if its been about an hour, get new credentials 
 								gcTime = time.localtime() # replace gcTime with current time
 								gc.login() # update credentials
@@ -146,40 +185,63 @@ while True: # run the monitoring function
 							sheet.append_row(data, value_input_option="USER_ENTERED")
 							del sheet
 						except Exception as e:
-							print('Datalogging failure.')
-							print(e)
-							print(traceback.format_exc())
+							if debug:
+								logging.info('Datalogging failure.')
+								logging.info(e)
+								logging.info(traceback.format_exc())
+
+								print('Datalogging failure.')
+								print(e)
+								print(traceback.format_exc())
 						
 						try:
 							# run the warning system here!
 							if config.sendAlerts:
 								warning_system(data, lastAlert, config)
 						except Exception as e:
-							print('Warning system failure.')
-							print(e)
-							print(traceback.format_exc())
+							if debug:
+								logging.info('Warning system failure.')
+								logging.info(e)
+								logging.info(traceback.format_exc())
+
+								print('Warning system failure.')
+								print(e)
+								print(traceback.format_exc())
 						
 						try: # update configuration
 							config = configuration(configFile)
 							debug = config.debug
 						except Exception as e:
-							print('Configuration update failure')
-							print(e)
-							print(traceback.format_exc())
+							if debug:
+								logging.info('Configuration update failure')
+								logging.info(e)
+								logging.info(traceback.format_exc())
+
+								print('Configuration update failure')
+								print(e)
+								print(traceback.format_exc())
 						
 						lastReading = minutes
 					else: 
 						pass
 			except Exception as e:
-				print('Display loop failure.')
-				print(e)
-				print(traceback.format_exc())
+				if debug:
+					logging.info('Display loop failure.')
+					logging.info(e)
+					logging.info(traceback.format_exc())
+
+					print('Display loop failure.')
+					print(e)
+					print(traceback.format_exc())
 			
 		time.sleep(0.4) # pause for 1 second
 	
 	except Exception as e:
-		print('Main loop failure.')
-		print(e)
-		print(traceback.format_exc())
-	
-	
+		if debug:
+			logging.info('Main loop failure.')
+			logging.info(e)
+			logging.info(traceback.format_exc())
+			
+			print('Main loop failure.')
+			print(e)
+			print(traceback.format_exc())
